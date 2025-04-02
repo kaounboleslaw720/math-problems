def684983b1dd5f2651bedebdcc5f3d0c5c6e502
kaounboleslaw720 <kaounboleslaw720@gmail.com>
2025-04-02 14:33:36 +0000

    Add 23.js

---
+++ src/23.js
@@ -0,0 +1,16 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function calculateGCD(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
