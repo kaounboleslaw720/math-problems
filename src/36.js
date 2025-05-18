75efc9e344208e1824ebddef96e541afaceb085e
kaounboleslaw720 <kaounboleslaw720@gmail.com>
2025-05-18 18:35:22 +0000

    Add 36.js

---
+++ src/36.js
@@ -0,0 +1,11 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function fibonacci(n) {
+  let fib = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[n];
+}
